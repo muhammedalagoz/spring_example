5ff1d89c20378cf6037c1d7ff7104b06cc7a4a42
muhammed <muhammed@muhammed-PU551LD>
2016-01-04 14:53:35 +0200

    add security

--- dnmSpring/src/com/istatistik/HelloController.java
+++ dnmSpring/src/com/istatistik/HelloController.java
@@ -4,14 +4,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/hello")
 public class HelloController {
 	
-	@RequestMapping(value="/index",method=RequestMethod.GET)
-	public String index(Model m) {
-		m.addAttribute("controlMessage","Bu message controllerdan");
+	@RequestMapping(value = { "/", "/welcome" }, method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("controlMessage","Bu message controllerdan");
 		return "hello";
 	}
+	
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public String adminPage(Model model) {
+
+		model.addAttribute("title", "Spring Security Custom Login Form");
+		model.addAttribute("message", "This is protected page!");
+
+		return "admin";
+
+	}
+
+	//Spring Security see this :
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(
+		@RequestParam(value = "error", required = false) String error,
+		@RequestParam(value = "logout", required = false) String logout,Model model) {
+
+		if (error != null) {
+			model.addAttribute("error", "Invalid username and password!");
+		}
+
+		if (logout != null) {
+			model.addAttribute("msg", "You've been logged out successfully.");
+		}
+
+		return "login";
+
+	}
+
 }
